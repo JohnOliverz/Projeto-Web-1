7257fd332df388d2686723f56d23a9b5f187f80a
Jhonatan Sousa <jhonatan-ce@hotmail.com>
2025-01-08 17:28:42 -0300

    :lipstick: feat: Componente do Rodapé

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import './Footer.css';
+
+export class Footer extends React.Component {
+    render() {
+        return (
+            <>
+                <section className="footer">
+                    <div className="footer-row">
+                        <div className="footer-col">
+                            <h4>Informações </h4>
+                            <ul className="links">
+                                <li><a href="#">Termos de Uso</a></li>
+                                <li><a href="#">Politica de Privacidade</a></li>
+                                <li>© 2025 IFCE - Campus Crato -  Web 1</li>
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+            </>
+        );
+    }
+
+}
+
+
+
+
+
