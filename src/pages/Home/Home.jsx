e212fee2722115449ebe28807b899466b66288ab
Jhonatan Sousa <jhonatan-ce@hotmail.com>
2025-01-08 17:25:01 -0300

    :recycle: refactor: Reestruturando o projeto

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,20 @@
+import { SideNavBar } from "../../components/sideNavBar/SideNavBar";
+import { Footer } from "../../components/footer/Footer";
+
+function Home(){
+    return(
+        <>
+
+        <SideNavBar />
+        <div id='divPrincipal'>
+
+
+          <div id='fixarRodape'><Footer /></div>
+
+        </div>
+
+      </>
+
+    )
+
+} export default Home;
