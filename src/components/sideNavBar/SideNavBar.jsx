485acb371bc335cc182e7adbff66fd3298cd4253
JeufoDev <llucasjefferson76@gmail.com>
2025-01-20 19:44:04 -0300

    ✨ feat: Estilização e adicionando link dos Favoritos

--- src/components/sideNavBar/SideNavBar.jsx
+++ src/components/sideNavBar/SideNavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './SideNavBar.css';
+import logo from '../../assets/bola2.svg';
 import { Link } from "react-router-dom";
 
 export class SideNavBar extends React.Component {
@@ -9,25 +10,24 @@ export class SideNavBar extends React.Component {
                 <aside className="sidebar">
                     <div className="sidebar-header">
 
-                    <span className="material-symbols-outlined" id="icon">circle</span><h2>Projeto</h2>
+                    <img className="logo" src={logo}/><h2>PlayerSpotlight</h2>
                     </div>
                     <ul className="sidebar-links">
                         <h4>
-                            <span>Menu Principal</span>
+                            <span id="span-color">Menu Principal</span>
                             <div className="menu-separator"></div>
                         </h4>
                         <li>
                             <Link to="/"><span className="material-symbols-outlined">home</span>Home</Link>
                             <Link to="/favoritos"><span className="material-symbols-outlined">star</span>Favoritos</Link>
+                            <Link to="#"><span className="material-symbols-outlined">info</span>Sobre</Link>
                         </li>
-
                     </ul>
 
                 </aside>
             </>
         );
     }
-
 }
 
 
