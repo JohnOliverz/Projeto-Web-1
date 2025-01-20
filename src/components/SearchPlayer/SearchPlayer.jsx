2876afbba49b442a96f69d5e3556e0adfedde4f6
RudhaEsmeraldo <rudhaesmeraldo@gmail.com>
2025-01-20 19:56:54 -0300

    ✨ feat: Estilização e Lógica  do SearchPlayer

--- src/components/SearchPlayer/SearchPlayer.jsx
+++ src/components/SearchPlayer/SearchPlayer.jsx
@@ -1,27 +1,32 @@
-// components/SearchPlayer.js
-import React, { useState } from 'react';
-import { buscarJogadores } from '../../services/Api';
+import React, { useState } from "react";
+import { buscarJogadores } from "../../services/Api";
+import "./SearchPlayer.css"; // Importa o CSS
 
 const SearchPlayer = ({ onResults }) => {
-  const [nome, setNome] = useState('');
+  const [nome, setNome] = useState("");
 
-  const handleSearch = async (e) => {
-    e.preventDefault();
+  const handleSearch = async () => {
+    if (!nome) {
+      alert("Por favor, insira o nome do jogador.");
+      return;
+    }
+
     const resultados = await buscarJogadores(nome);
-    onResults(resultados);
+    onResults(resultados); // Passa os resultados para o componente pai
   };
 
   return (
-    <div>
-      <form onSubmit={handleSearch}>
-        <input
-          type="text"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-          placeholder="Digite o nome do jogador"
-        />
-        <button type="submit">Buscar</button>
-      </form>
+    <div className="search-container">
+      <input
+        type="text"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+        placeholder="Digite o nome do jogador"
+        className="search-input"
+      />
+      <button onClick={handleSearch} className="search-button">
+      <span className="material-symbols-outlined">search</span>
+      </button>
     </div>
   );
 };
