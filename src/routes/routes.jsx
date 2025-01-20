2471c63472bde94cda5f880523d2e61a108ae5f8
Jhonatan Sousa <jhonatan-ce@hotmail.com>
2025-01-19 22:37:36 -0300

    ♻️ refactor: Nova rota

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,13 +1,24 @@
+// routes.jsx
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import Favoritos from "../pages/Favorites/Favorites";
 
-function AppRoutes(){
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>} ></Route>
-            </Routes>
-        </BrowserRouter>
-    )
+function AppRoutes() {
+  const [favoritos, setFavoritos] = useState([]);
+  const [jogadores, setJogadores] = useState([]); // Defina aqui se for global
 
-} export default AppRoutes
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home setFavoritos={setFavoritos} favoritos={favoritos} setJogadores={setJogadores} />}
+        />
+        <Route path="/favoritos" element={<Favoritos favoritos={favoritos} />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
